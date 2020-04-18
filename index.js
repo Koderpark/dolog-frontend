8bde4504b77af9749ba193ff6a248ad1bad52328
박성훈 / Koder <koder0205@gmail.com>
2020-04-18 16:03:04 +0900

    update
    왼쪽 고정바 완성

---
+++ index.js
@@ -0,0 +1,8 @@
+function overlay_out(){
+    document.getElementById('header').style.display = 'block';
+    document.getElementById('mobileoverlay').style.display = 'block';
+}
+function overlay_in(){
+    document.getElementById('header').style.display = '';
+    document.getElementById('mobileoverlay').style.display = '';
+}
